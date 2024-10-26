7b9ee5e9bce4ae0c8468b8b7a4c7382884e87d29
amankmr14 <amankmr765@gmail.com>
2024-10-26 21:29:11 +0530

    ENV added

--- register.php
+++ register.php
@@ -1,63 +1,29 @@
 <?php
 require 'vendor/autoload.php';
-require 'db.php';
 
+use MongoDB\Client;
+use MongoDB\Exception\Exception as MongoDBException;
 use MongoDB\BSON\UTCDateTime;
+use Dotenv\Dotenv;
 
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    echo json_encode(["error" => "Only POST requests are allowed."]);
-    exit();
-}
+$dotenv = Dotenv::createImmutable(_DIR_);
+$dotenv->load();
 
-$data = json_decode(file_get_contents('php://input'), true);
+function getMongoDBConnection()
+{
+    $uri = $_ENV['MONGO_URI'] ?? '';
 
-if (!isset($data['name'], $data['time'], $data['stars'])) {
- http_response_code(400);
- echo json_encode(["error" => "All fields (name, time, stars) are required and must be valid."]);
- exit();
-}
-
-$name = htmlspecialchars(trim($data['name']));
-$time = filter_var($data['time'], FILTER_VALIDATE_INT);
-$stars = filter_var($data['stars'], FILTER_VALIDATE_INT);
-
-try {
-    $client = getMongoDBConnection();
-    $collection = $client->selectDatabase('sky-angel')->score;
-
-    $userData = [
-        "name" => $name,
-        "time" => $time,
-        "stars" => $stars,
-        "created_at" => new MongoDB\BSON\UTCDateTime()
-    ];
-
-    $insertResult = $collection->insertOne($userData);
-
-    $allUsers = $collection->find()->toArray();
-
-    $responseData = array_map(function ($user) {
-        return [
-            'name' => $user['name'],
-            'time' => $user['time'],
-            'stars' => $user['stars'],
-            'id' => $user['_id']
-        ];
-    }, $allUsers);
-
-    http_response_code(200);
-    echo json_encode([
-        "message" => "User registered successfully.",
-        "data" => $responseData
-    ]);
-
-} catch (Exception $e) {
-    http_response_code(500);
-    echo json_encode(["error" => "Could not save data to MongoDB.", "details" => $e->getMessage()]);
+    if (!$uri) {
+        die("Error: MONGO_URI is not set. Check your .env file.");
+    }
+    try {
+        $client = new Client($uri);
+        $client->selectDatabase('admin')->command(['ping' => 1]);
+        return $client;
+    } catch (MongoDBException $e) {
+        die("MongoDB error: " . $e->getMessage());
+    } catch (Exception $e) {
+        die("General error: " . $e->getMessage());
+    }
 }
