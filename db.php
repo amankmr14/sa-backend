dc26798f7bd56303a1f3340376c1539994221ad8
amankmr14 <amankmr765@gmail.com>
2024-10-27 10:09:02 +0530

    env

--- db.php
+++ db.php
@@ -4,17 +4,26 @@ require 'vendor/autoload.php';
 use MongoDB\Client;
 use MongoDB\Exception\Exception as MongoDBException;
 use MongoDB\BSON\UTCDateTime;
+use Dotenv\Dotenv;
 
-function getMongoDBConnection() {
-    $uri = 'mongodb+srv://amankmr765:aman123@lms-0.dgc8vq1.mongodb.net/?retryWrites=true&w=majority&appName=lms-0';
 
-    try {
-        $client = new Client($uri);
-        $client->selectDatabase('admin')->command(['ping' => 1]);
-        return $client;
-    } catch (MongoDBException $e) {
-        die("MongoDB error: " . $e->getMessage());
-    } catch (Exception $e) {
-        die("General error: " . $e->getMessage());
-    }
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+function getMongoDBConnection()
+{
+ $uri = $_ENV['MONGO_URI'] ?? '';
+
+ if (!$uri) {
+  die("Error: MONGO_URI is not set. Check your .env file.");
+ }
+ try {
+  $client = new Client($uri);
+  $client->selectDatabase('admin')->command(['ping' => 1]);
+  return $client;
+ } catch (MongoDBException $e) {
+  die("MongoDB error: " . $e->getMessage());
+ } catch (Exception $e) {
+  die("General error: " . $e->getMessage());
+ }
 }
